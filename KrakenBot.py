0f19fa3e04348f2557ea585e50ea7d4f60e31112
szymciem8 <szymciem@DESKTOP-Szymon.localdomain>
2021-11-29 14:22:18 +0100

    new update

--- KrakenBot.py
+++ KrakenBot.py
@@ -7,22 +7,52 @@ import time
 from requests.models import parse_header_links
 import logging
 import os
+import pickle
+import datetime
+
+# Time constants
+MINUTE=60
+HOUR=MINUTE*60
+DAY=HOUR*24
+
+MONEY_INVESTED_PATH = path = os.path.join(os.path.dirname(__file__), 'data/money_invested.pickle')
+START_TIME_PATH = os.path.join(os.path.dirname(__file__), 'logs/kraken_bot_log.log')
 
 format  = '%(asctime)s-%(process)d-%(levelname)s-%(message)s'
-path = os.path.join(os.path.dirname(__file__), 'logs/kraken_bot_log.log')
-logging.basicConfig(filename=path, filemode='a', format=format, level=logging.INFO)
+logging.basicConfig(filename=START_TIME_PATH, filemode='a', format=format, level=logging.INFO)
 
 class KrakenBot:
 
     def __init__(self, api_url, api_key, api_sec):
         self.BASE_CURRENCY='ZUSD'
-
+   
         self.api_url=api_url
         self.api_key=api_key
         self.api_sec=api_sec
 
-    def dollar_cost_average(self):
-        pass
+        if os.path.exists(MONEY_INVESTED_PATH):
+            with open(MONEY_INVESTED_PATH, 'wb') as file:
+                self.money_invested = pickle.load(file)
+        else:
+            self.money_invested=0
+
+    def get_kraken_signature(self, urlpath, data, secret):
+        postdata = urllib.parse.urlencode(data)
+        encoded = (str(data['nonce']) + postdata).encode()
+        message = urlpath.encode() + hashlib.sha256(encoded).digest()
+
+        mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+        sigdigest = base64.b64encode(mac.digest())
+        return sigdigest.decode()
+
+    # Attaches auth headers and returns results of a POST request
+    def kraken_request(self, uri_path, data, api_key, api_sec):
+        headers = {}
+        headers['API-Key'] = api_key
+        # get_kraken_signature() as defined in the 'Authentication' section
+        headers['API-Sign'] = self.get_kraken_signature(uri_path, data, api_sec)             
+        req = requests.post((self.api_url + uri_path), headers=headers, data=data)
+        return req
 
     def get_balance(self, token=None):
         '''
@@ -40,9 +70,32 @@ class KrakenBot:
             except:
                 logging.warning("This token does not exists!")
 
+    def get_price(self, pair, type='ask'):
+        '''
+        Get price of specified asset.
+        '''
+        resp = requests.get(self.api_url+'/0/public/Ticker?pair='+pair)
+        price = float(resp.json()['result'][pair][type[0]][0])
+
+        return price
+
+    def order_min(self, pair):
+        '''
+        Returns the minimum order value of give asset.
+        '''
+        resp = requests.get(self.api_url+'/0/public/AssetPairs?pair='+pair)
+        return float(resp.json()['result'][pair]['ordermin'])
+
+    def dec_places(self, pair):
+        '''
+        Returns maximum number of decimal places of given asset.
+        '''
+        resp = requests.get(self.api_url+'/0/public/AssetPairs?pair='+pair)
+        return int(resp.json()['result'][pair]['lot_decimals'])
+
     def buy_pair(self, pair, ordertype, volume, price=None):
         '''
-        Buy given asset with specifed currency. 
+        Buy given asset with specifed settings. 
         '''
         if volume < self.order_min(pair):
             logging.warning("Volume is too low!")
@@ -56,12 +109,17 @@ class KrakenBot:
                     "volume": volume,
                     "pair": pair
                 }, self.api_key, self.api_sec)
+
+                if not resp.json()['error']:
+                    self.money_invested += volume * self.get_price(pair)
             
                 return resp.json()
         return 1
 
     def sell_pair(self, pair, ordertype, volume):
-
+        '''
+        Sell given asset with specified settings.
+        '''
         if volume < self.order_min(pair):
             logging.warning("Volume is too low!")
             return 1
@@ -78,24 +136,10 @@ class KrakenBot:
                 return resp.json()
         return 1
 
-    def order_min(self, pair):
-        resp = requests.get(self.api_url+'/0/public/AssetPairs?pair='+pair)
-
-        return float(resp.json()['result'][pair]['ordermin'])
-
-    def dec_places(self, pair):
-        resp = requests.get(self.api_url+'/0/public/AssetPairs?pair='+pair)
-
-        return int(resp.json()['result'][pair]['lot_decimals'])
-
-    def get_price(self, pair, type='ask'):
-
-        resp = requests.get(self.api_url+'/0/public/Ticker?pair='+pair)
-        price = float(resp.json()['result'][pair][type[0]][0])
-
-        return price
-
     def stake(self, crypto=None, amount=None):
+        '''
+        Stake list of assets or just one asset.
+        '''
         if isinstance(crypto, list):
             logging.info('Staking assets')
             for asset in crypto:
@@ -120,7 +164,9 @@ class KrakenBot:
             return resp.json()
 
     def unstake(self, asset):
-        
+        '''
+        Unstake given asset.
+        '''
         method = self.get_staking_info(asset)['method']
 
         asset += ".S"
@@ -135,10 +181,10 @@ class KrakenBot:
 
         return resp.json()
 
-    def get_staked_assets_balance(self):
-        pass
-
     def get_staking_info(self, asset):
+        '''
+        Get staking info of given asset.
+        '''
         resp = self.kraken_request('/0/private/Staking/Assets', {
         "nonce": str(int(1000*time.time()))
         }, self.api_key, self.api_sec)
@@ -152,6 +198,10 @@ class KrakenBot:
         return 1
 
     def check_contrib_values(self, pairs, contrib_per_period, mode='min_order'):
+        '''
+        Check if contribution values of each asset or higher than minimum 
+        order value. If not, change them accordigly to specified mode. 
+        '''
         contributions = {}
         for pair, percentage in pairs.items():
             contrib = (percentage * contrib_per_period)/100
@@ -176,33 +226,53 @@ class KrakenBot:
 
         return contributions
 
-    def make_contribution(self, pairs, contrib_per_period):
-        contributions = self.check_contrib_values(pairs, contrib_per_period)
+    def get_start_time(self):
+        '''
+        Read start time of the bot from pickle file, if it does not exist, create one. 
+        '''
+        path = os.path.join(os.path.dirname(__file__), 'start.pickle')
+        if os.path.exists(START_TIME_PATH):
+            with open(START_TIME_PATH, 'rb') as file:
+                start = pickle.load(file)
+            logging.info('Reading last cotribution date...')
+            existed=True
+        else:
+            logging.info('Pickle file does not exist.')
+            logging.info('Creating new one.')
+            # Save with pickle
+            start = datetime.now()
+            with open(START_TIME_PATH, 'wb') as file:
+                pickle.dump(start, file)
+            existed=False
 
+        return start, existed
+
+    def make_contribution(self, pairs, contrib_per_period):
+        '''
+        Make regular contribution of asset. 
+        '''
+        contributions = self.check_contrib_values(pairs, contrib_per_period)
         logging.info('Buying assets')
         for pair, value in contributions.items():
             if self.get_balance('ZUSD') < value:
                 logging.warning(f'Low balance! Bying {pair} "failed.')
             else:
                 # kraken_bot.buy_pair(pair, 'market', contributions[pair])
+                with open(MONEY_INVESTED_PATH, 'wb') as file:
+                    pickle.dump(self.money_invested, file)
+
                 logging.info(f'Bought pair: {pair} {value}->{round(value*self.get_price(pair),2)}')
+    
+    def get_profit(type='overall'):
+        # profit = value_of_assets/value_of_money_invested
+        # What if something is sold?
+        pass
 
+    def get_staked_assets_balance(self):
+        pass
 
-    def get_kraken_signature(self, urlpath, data, secret):
+    def expected_staking_profit(self, timeframe='month'):
+        pass
 
-        postdata = urllib.parse.urlencode(data)
-        encoded = (str(data['nonce']) + postdata).encode()
-        message = urlpath.encode() + hashlib.sha256(encoded).digest()
-
-        mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
-        sigdigest = base64.b64encode(mac.digest())
-        return sigdigest.decode()
-
-    # Attaches auth headers and returns results of a POST request
-    def kraken_request(self, uri_path, data, api_key, api_sec):
-        headers = {}
-        headers['API-Key'] = api_key
-        # get_kraken_signature() as defined in the 'Authentication' section
-        headers['API-Sign'] = self.get_kraken_signature(uri_path, data, api_sec)             
-        req = requests.post((self.api_url + uri_path), headers=headers, data=data)
-        return req
+    def dollar_cost_average(self):
+        pass
--- main.py
+++ main.py
@@ -6,16 +6,10 @@ from datetime import datetime
 import pickle
 import logging
 
-# Time constants
-MINUTE=60
-HOUR=MINUTE*60
-DAY=HOUR*24
-
 FREQUENCY_D=30              #DAYS
 FREQUENCY_H=FREQUENCY_D*24  #HOURS
 FREQUENCY_M=FREQUENCY_H*60  #MINUTES
 FREQUENCY_S=FREQUENCY_H*60  #SECONDS
-CONTRIBUTION=40             #DOLLARS
 
 # Logging
 format  = '%(asctime)s-%(process)d-%(levelname)s-%(message)s'
@@ -32,30 +26,18 @@ kraken_bot = KrakenBot(api_url, api_key, api_sec)
 # Value of contribution each period
 contrib_per_period = 40
 
-# Proprtions are in %
+# Paris with proportions. Proprtions are in %
 pairs = {'XETHZUSD':36, 'ADAUSD':22,  'DOTUSD':22, 'ALGOUSD':10, 'KSMUSD':10}
 staked_assets = ['ADA', 'DOT', 'KSM', 'ALGO']
 
 check_sum = 0
 for key, val in pairs.items(): check_sum += val
-
 if check_sum != 100:
     logging.info('Assets proportions are not correct!')
     sys.exit(0)
 
-path = os.path.join(os.path.dirname(__file__), 'start.pickle')
-if os.path.exists(path):
-    with open(path, 'rb') as start_file:
-        start = pickle.load(start_file)
-    logging.info('Reading last cotribution date...')
-else:
-    logging.info('Pickle file does not exist.')
-    logging.info('Creating new one.')
-    # Save with pickle
-    start = datetime.now()
-    with open(path, 'wb') as start_file:
-        pickle.dump(start, start_file)
-
+start, existed = kraken_bot.get_start_time()
+if not existed:
     kraken_bot.buy_pair(pairs, contrib_per_period)
     time.sleep(MINUTE)
     kraken_bot.stake(staked_assets)
@@ -71,12 +53,14 @@ while True:
     
     # Make contribution
     kraken_bot.make_contribution(pairs, contrib_per_period)
+    logging.info('New contribution has been made')
+
+    # time.sleep(MINUTE)
 
     # Stake available assets
     kraken_bot.stake(staked_assets)
-
-    logging.info('New contribution has been made')
-
+    logging.info('Assets has been staked')
+    
     start = datetime.now()
-    with open(r'start.pickle', 'wb') as start_file:
+    with open(path, 'wb') as start_file:
         pickle.dump(start, start_file)
